cb7a207c089d7577a240a06a34611826bb61c45a
Vivek Singh <vivek.singh@liqvid.com>
2023-07-31 22:58:11 +0530

    images added , differentiator added

--- src/Layouts/Home.tsx
+++ src/Layouts/Home.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 import CallToAction from '../Components/CallToAction'
 import { useNavigate } from 'react-router'
 import '../Styles/Image.css'
+import Differentiator from '../Components/Differentiator'
 
 function Home() {
   return (
     <>
       <HeadingDisplay />
+      <Divider sx={{ backgroundColor: '#808080', marginBottom: '20px' }} />
+      <Differentiator/>
       <Divider sx={{ backgroundColor: '#808080', marginBottom: '20px' }} />
       <MainBody />
       <CallToAction />
@@ -35,16 +38,19 @@ function HeadingDisplay() {
         width={isSmallScreen === false ? '40%' : '100%'}
         alt='Hero'
         src='/hero.png' />
-      <Box textAlign={'end'} width={isSmallScreen === false ? '40%' : '100%'}  >
+      <Box textAlign={'start'} width={isSmallScreen === false ? '40%' : '100%'}  >
         <Typography fontSize={40} >
           We make sense of your
           <br />
           feedback.
         </Typography>
-        <Typography color={'#808080'} >
-          With FeedbackSense, you can gain valuable insights into customer sentiment and feedback, and use that information to improve your products or services.
+        <Typography fontSize={20} color={'#808080'} >
+          Gain valuable insights into customer sentiment and feedback,
+          and use that information to improve your products or services
+          with FeedbackSense.
+          <br />
         </Typography>
-        <Box marginTop={'10px'} >
+        <Box marginTop={'50px'} >
           <button onClick={() => navigate('/product')} style={{ marginRight: '10px' }} className='outlined-button' >Learn more</button>
           <button onClick={handleStartFreePlan} className='contained-button' >Start a free plan</button>
         </Box>
@@ -54,6 +60,8 @@ function HeadingDisplay() {
 }
 
 function MainBody() {
+
+  const isSmallScreen = useMediaQuery('(max-width: 830px)');
 
   return (
     <Box >
@@ -66,37 +74,66 @@ function MainBody() {
         design='image-shadow-3'
         desc={'FeedbackSense simplifies the feedback collection process by providing businesses with a centralized platform to gather, organize, and analyze feedback from their customers.'}
       />
-      <RightDisplay
-        title={'Data-Driven Decision Making'}
-        img='/dragdropwithflow.png'
-        design='image-shadow-3'
-        desc={'Identify emerging trends, spot areas for improvement, and make informed decisions based on data, enabling your business to stay ahead of the competition.'}
-      />
+
+      {
+        isSmallScreen === false ?
+          <RightDisplay
+            title={'Data-Driven Decision Making'}
+            img='/dragdropwithflow.png'
+            design='image-shadow-3'
+            desc={'Identify emerging trends, spot areas for improvement, and make informed decisions based on data, enabling your business to stay ahead of the competition.'}
+          /> :
+          <LeftDisplay
+            title={'Data-Driven Decision Making'}
+            img='/dragdropwithflow.png'
+            design='image-shadow-3'
+            desc={'Identify emerging trends, spot areas for improvement, and make informed decisions based on data, enabling your business to stay ahead of the competition.'}
+          />
+      }
       <LeftDisplay
         title={'Enhanced Customer Engagement'}
         img='/Enhanced customer engagement.png'
         design='image-shadow-3'
         desc={'With FeedbackSense, businesses can actively engage with their customers and demonstrate that their opinions matter.'}
       />
-      <RightDisplay
-        title={'Excellent Customer Support'}
-        img='/Eccelent customer support.png'
-        design='image-shadow-3'
-        desc={'At FeedbackSense, we pride ourselves on delivering exceptional customer support. Our dedicated team is ready to assist you with any questions or issues you may have.'}
-      />
+      {
+        isSmallScreen === false ?
+          <RightDisplay
+            title={'Excellent Customer Support'}
+            // img='/Eccelent customer support.png'
+            img='/customer-support.jpg'
+            design='image-shadow-3'
+            desc={'At FeedbackSense, we pride ourselves on delivering exceptional customer support. Our dedicated team is ready to assist you with any questions or issues you may have.'}
+          /> :
+          <LeftDisplay
+            title={'Excellent Customer Support'}
+            // img='/Eccelent customer support.png'
+            img='/customer-support.jpg'
+            design='image-shadow-3'
+            desc={'At FeedbackSense, we pride ourselves on delivering exceptional customer support. Our dedicated team is ready to assist you with any questions or issues you may have.'}
+          />
+      }
       <LeftDisplay
         title={'Efficient Lead Generation'}
         img='/Efficient lead generation.png'
         design='image-shadow-3'
         desc={'FeedbackSense not only helps you gather feedback but also acts as a lead-generation tool.'}
       />
-      <RightDisplay
-        title={'Effortless Survey Creation'}
-        // img='/effortless survey creation.png'
-        img='analysis5.png'
-        design='image-shadow-3'
-        desc={'Our platform is designed to make survey creation a breeze. With our intuitive interface and user-friendly features, you can quickly build engaging and impactful surveys without any technical expertise.'}
-      />
+      {
+        isSmallScreen === false ?
+          <RightDisplay
+            title={'Effortless Survey Creation'}
+            img='analysis5.png'
+            design='image-shadow-3'
+            desc={'Our platform is designed to make survey creation a breeze. With our intuitive interface and user-friendly features, you can quickly build engaging and impactful surveys without any technical expertise.'}
+          /> :
+          <LeftDisplay
+            title={'Effortless Survey Creation'}
+            img='analysis5.png'
+            design='image-shadow-3'
+            desc={'Our platform is designed to make survey creation a breeze. With our intuitive interface and user-friendly features, you can quickly build engaging and impactful surveys without any technical expertise.'}
+          />
+      }
     </Box>
   )
 }
@@ -106,7 +143,7 @@ export function RightDisplay({ title, desc, img, design }: any) {
   const isSmallScreen = useMediaQuery('(max-width: 830px)');
 
   return (
-    <Box sx={{ padding: '10%', display: isSmallScreen === false ? 'flex' : 'block', justifyContent: 'space-between' }} >
+    <Box sx={{ padding: isSmallScreen === false ? '5%' : '10%', display: isSmallScreen === false ? 'flex' : 'block', justifyContent: 'space-between' }} >
       <img
         className={design}
         style={{ borderRadius: '10px' }}
@@ -117,7 +154,7 @@ export function RightDisplay({ title, desc, img, design }: any) {
         <Typography fontSize={40} >
           {title}
         </Typography>
-        <Typography color={'#808080'} >
+        <Typography fontSize={20} color={'#808080'} >
           {desc}
         </Typography>
       </Box>
@@ -125,15 +162,15 @@ export function RightDisplay({ title, desc, img, design }: any) {
   )
 }
 
-export function LeftDisplay({ title, desc, img,design }: any) {
+export function LeftDisplay({ title, desc, img, design }: any) {
   const isSmallScreen = useMediaQuery('(max-width: 830px)');
   return (
-    <Box sx={{ padding: '10%', display: isSmallScreen === false ? 'flex' : 'block', justifyContent: 'space-between' }} >
+    <Box sx={{ padding: isSmallScreen === false ? '5%' : '10%', display: isSmallScreen === false ? 'flex' : 'block', justifyContent: 'space-between' }} >
       <Box textAlign={'start'} width={isSmallScreen === false ? '40%' : '100%'}  >
         <Typography fontSize={40} >
           {title}
         </Typography>
-        <Typography color={'#808080'} >
+        <Typography fontSize={20} color={'#808080'} >
           {desc}
         </Typography>
       </Box>
