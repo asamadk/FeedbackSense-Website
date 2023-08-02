310c757fc58a763e086f61e7332c19441bace39b
Vivek Singh <vivek.singh@liqvid.com>
2023-08-03 01:34:38 +0530

    FeedbackSense  Branding

--- src/Layouts/Pricing.tsx
+++ src/Layouts/Pricing.tsx
@@ -219,6 +219,12 @@ function PlanDetailsTable({ starter, ultimate }: any) {
                         <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
                     </TableRow>
                     <TableRow>
+                        <TableCell>FeedbackSense  Branding</TableCell>
+                        <TableCell><RemoveIcon /></TableCell>
+                        <TableCell><RemoveIcon /></TableCell>
+                        <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
+                    </TableRow>
+                    <TableRow>
                         <TableCell>Questions in survey</TableCell>
                         <TableCell>Unlimited</TableCell>
                         <TableCell>Unlimited</TableCell>
