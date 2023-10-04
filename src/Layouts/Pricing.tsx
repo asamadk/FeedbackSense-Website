288ae7f927055837e0cdebaa24d288fa03110184
Vivek Singh <vivek.singh@liqvid.com>
2023-10-04 22:17:13 +0530

    pricing updated

--- src/Layouts/Pricing.tsx
+++ src/Layouts/Pricing.tsx
@@ -1,10 +1,13 @@
-import { Box, Button, Card, CardContent, CardHeader, Container, Grid, Link, Modal, Paper, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, ThemeProvider, Typography, createTheme, makeStyles, useMediaQuery } from '@mui/material';
+import { Box, Button, Card, CardContent, CardHeader, Container, Grid, Link, MenuItem, Modal, Paper, Select, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, ThemeProvider, Typography, createTheme, duration, makeStyles, useMediaQuery } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 import RemoveIcon from '@mui/icons-material/Remove';
 import Typewriter from "typewriter-effect";
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { LoadingButton } from '@mui/lab';
+import { featureList } from '../Utils/PriceConstants';
+import axios from 'axios';
+import FSLoader from '../Components/FSLoader';
 
 const darkTheme = createTheme({
   palette: {
@@ -17,11 +20,15 @@ const darkTheme = createTheme({
 
 export default function Pricing(props: any) {
 
-  const [starterPrice, setStarterPrice] = useState(25);
-  const [ultimatePrice, setUltimatePrice] = useState(49);
+  const [starterPrice, setStarterPrice] = useState(12);
+  const [plusPrice, setPlusPrice] = useState(22);
+  const [ultimatePrice, setUltimatePrice] = useState(39);
   const [billing, setBilling] = useState<'month' | 'year'>('year');
   const [showModal, setShowModal] = useState(false);
   const [selectedPrice, setSelectedPrice] = useState<null | string>(null);
+  const [currency, setCurrency] = useState<'usd' | 'inr'>('usd');
+  const [curSym, setCurSym] = useState<'$' | '₹'>('$');
+  const [loading,setLoading] = useState(false);
 
   const content = {
     'header-p1': 'Simple no-tricks pricing',
@@ -29,30 +36,30 @@ export default function Pricing(props: any) {
     'description': 'Plan doesn\'t fits your needs? Contact us to discuss customized solutions.',
     'option1': 'Monthly',
     'option2': 'Annual',
-    '01_title': 'FeedbackSense Free',
-    '01_price': '$0',
-    '01_suffix': `/mo`,
-    '01_benefit1': '1 Active survey',
+    '01_title': 'Starter',
+    '01_price': curSym + starterPrice,
+    '01_suffix': `/mo (billed ${billing}ly)`,
+    '01_benefit1': '3 Active surveys',
     '01_benefit2': '1 User',
-    '01_benefit3': '50 Response / month',
+    '01_benefit3': '100 Response / month',
     '01_benefit4': 'Basic analysis',
     '01_primary-action': 'Select plan',
     '01_secondary-action': 'Learn more',
-    '02_title': 'FeedbackSense Starter',
-    '02_price': '$' + starterPrice,
+    '02_title': 'Plus',
+    '02_price': curSym + plusPrice,
     '02_suffix': `/mo (billed ${billing}ly)`,
     '02_benefit1': '5 Active Surveys',
-    '02_benefit2': 'Unlimited users',
+    '02_benefit2': '3 users',
     '02_benefit3': '2000 Response / month',
     '02_benefit4': 'Detailed analysis',
     '02_primary-action': 'Select plan',
     '02_secondary-action': 'Learn more',
-    '03_title': 'FeedbackSense Ultimate',
-    '03_price': '$' + ultimatePrice,
+    '03_title': 'Business',
+    '03_price': curSym + ultimatePrice,
     '03_suffix': `/mo (billed ${billing}ly)`,
     '03_benefit1': '10 Active Surveys',
-    '03_benefit2': 'User management panel',
-    '03_benefit3': '5000 Response / month',
+    '03_benefit2': '10 Users',
+    '03_benefit3': '10000 Response / month',
     '03_benefit4': 'AI assisted analysis',
     '03_primary-action': 'Select Plan',
     '03_secondary-action': 'Learn more',
@@ -60,15 +67,62 @@ export default function Pricing(props: any) {
     ...props.content
   };
 
+  let init = false;
+
+  useEffect(() => {
+    if (init === true) { return; }
+    init = true;
+    fetchUserCountry();
+  }, []);
+
+  useMemo(() => {
+    if (currency === 'usd') {
+      setCurSym('$');
+      if (billing === 'year') {
+        setStarterPrice(12);
+        setPlusPrice(22);
+        setUltimatePrice(39);
+      } else {
+        setStarterPrice(16);
+        setPlusPrice(26);
+        setUltimatePrice(45);
+      }
+    } else {
+      setCurSym('₹');
+      if (billing === 'year') {
+        setStarterPrice(499);
+        setPlusPrice(999);
+        setUltimatePrice(1799);
+      } else {
+        setStarterPrice(699);
+        setPlusPrice(1199);
+        setUltimatePrice(1999);
+      }
+    }
+  }, [currency, billing]);
+
+  const fetchUserCountry = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get('https://ipapi.co/json/');
+      setLoading(false);
+      const countryCode = data?.country_code;
+      if (countryCode === 'IN') {
+        setCurrency('inr')
+      } else {
+        setCurrency('usd');
+      }
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  }
+
   const handleChange = (event: any) => {
     if (event.target.checked === true) {
       setBilling('year');
-      setStarterPrice(25);
-      setUltimatePrice(49);
     } else {
       setBilling('month');
-      setStarterPrice(29);
-      setUltimatePrice(55);
     }
   };
 
@@ -81,6 +135,10 @@ export default function Pricing(props: any) {
     setShowModal(false);
   }
 
+  const handleCurrencyChange = (event: any) => {
+    setCurrency(event.target.value);
+  }
+
   return (
     <section>
       <Container maxWidth="lg">
@@ -89,14 +147,43 @@ export default function Pricing(props: any) {
             <Container maxWidth="sm">
               <Typography variant="h3" component="h2" gutterBottom={true}>
                 <Typography variant="h3" component="span" color="primary">{content['header-p1']} </Typography>
+                <Box sx={{ fontSize: '30px', color: '#006b38ff' }} >
+                  <Typewriter
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString("")
+                        .pauseFor(500)
+                        .deleteAll()
+                        .typeString(content['free-trial'])
+                        .start()
+                    }}
+                  />
+                </Box>
               </Typography>
               <Typography sx={{ color: '#808080' }} variant="subtitle1" color="textSecondary" paragraph={true}>{content['description']}</Typography>
-
-              <div>
-                <Typography variant="subtitle1" component="span">{content['option1']}</Typography>
-                &nbsp; <Switch name="checkbox" color="primary" checked={billing === 'year'} onChange={handleChange} /> &nbsp;
-                <Typography variant="subtitle1" component="span">{content['option2']}</Typography>
-              </div>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between' }} >
+                <div>
+                  <Typography variant="subtitle1" component="span">{content['option1']}</Typography>
+                  &nbsp; <Switch name="checkbox" color="primary" checked={billing === 'year'} onChange={handleChange} /> &nbsp;
+                  <Typography variant="subtitle1" component="span">{content['option2']}</Typography>
+                </div>
+                <div>
+                  <ThemeProvider theme={darkTheme} >
+                    <label style={{ marginRight: '10px' }} >Currency</label>
+                    <Select
+                      sx={{ width: '100px' }}
+                      labelId="demo-select-small-label"
+                      id="demo-select-small"
+                      size='small'
+                      value={currency}
+                      onChange={handleCurrencyChange}
+                    >
+                      <MenuItem value={'usd'}>USD</MenuItem>
+                      <MenuItem value={'inr'}>INR</MenuItem>
+                    </Select>
+                  </ThemeProvider>
+                </div>
+              </Box>
             </Container>
           </Box>
           <Grid container spacing={3} >
@@ -108,18 +195,6 @@ export default function Pricing(props: any) {
                     <Typography color={'#006dff'} variant="h3" component="h2" gutterBottom={true}>
                       {content['01_price']}
                       <Typography variant="h6" component="span">{content['01_suffix']}</Typography>
-                      <Box sx={{ fontSize: '15px', color: '#8F384D' }} >
-                        <Typewriter
-                          onInit={(typewriter) => {
-                            typewriter
-                              .typeString("")
-                              .pauseFor(1000)
-                              .deleteAll()
-                              .typeString(content['free-trial'])
-                              .start()
-                          }}
-                        />
-                      </Box>
                     </Typography>
                     <Typography color={'#808080'} variant="subtitle1" component="p">{content['01_benefit1']}</Typography>
                     <Typography color={'#808080'} variant="subtitle1" component="p">{content['01_benefit2']}</Typography>
@@ -138,18 +213,6 @@ export default function Pricing(props: any) {
                     <Typography color={'#006dff'} variant="h3" component="h2" gutterBottom={true}>
                       {content['02_price']}
                       <Typography variant="h6" component="span">{content['02_suffix']}</Typography>
-                      <Box sx={{ fontSize: '15px', color: '#8F384D' }} >
-                        <Typewriter
-                          onInit={(typewriter) => {
-                            typewriter
-                              .typeString("")
-                              .pauseFor(1000)
-                              .deleteAll()
-                              .typeString(content['free-trial'])
-                              .start()
-                          }}
-                        />
-                      </Box>
                     </Typography>
                     <Typography color={'#808080'} variant="subtitle1" component="p">{content['02_benefit1']}</Typography>
                     <Typography color={'#808080'} variant="subtitle1" component="p">{content['02_benefit2']}</Typography>
@@ -168,18 +231,6 @@ export default function Pricing(props: any) {
                     <Typography variant="h3" color={'#006dff'} component="h2" gutterBottom={true}>
                       {content['03_price']}
                       <Typography variant="h6" component="span">{content['03_suffix']}</Typography>
-                      <Box sx={{ fontSize: '15px', color: '#8F384D' }} >
-                        <Typewriter
-                          onInit={(typewriter) => {
-                            typewriter
-                              .typeString("")
-                              .pauseFor(1000)
-                              .deleteAll()
-                              .typeString(content['free-trial'])
-                              .start()
-                          }}
-                        />
-                      </Box>
                     </Typography>
                     <Typography color={'#808080'} variant="subtitle1" component="p">{content['03_benefit1']}</Typography>
                     <Typography color={'#808080'} variant="subtitle1" component="p">{content['03_benefit2']}</Typography>
@@ -196,12 +247,16 @@ export default function Pricing(props: any) {
       <ThemeProvider theme={darkTheme} >
         <Box margin={'20px'} >
           <PlanDetailsTable
+            curSym={curSym}
+            plus={plusPrice}
             starter={starterPrice}
             ultimate={ultimatePrice}
+            billing={billing}
           />
         </Box>
       </ThemeProvider>
-      <EmailModal price={selectedPrice} open={showModal} close={handleModalClose} />
+      <EmailModal currency={currency} price={selectedPrice} open={showModal} close={handleModalClose} />
+      <FSLoader show={loading} />
     </section>
   );
 }
@@ -224,9 +279,9 @@ function EmailModal(props: any) {
   };
 
   const handlePricingClick = async () => {
-    if(email == null || email.length < 1){return;}
+    if (email == null || email.length < 1) { return; }
     setLoading(true);
-    const message = `Email : ${email} Selected Price ${props.price}`;
+    const message = `Email : ${email} Selected Price ${props.price} using currency ${props.currency}`;
     await emailjs.send("service_vl2wgl3", "template_ed76ptf", {
       message: message,
       from_email: 'founder@feedbacksense.io',
@@ -269,115 +324,43 @@ function EmailModal(props: any) {
   )
 }
 
-function PlanDetailsTable({ starter, ultimate }: any) {
+function PlanDetailsTable({ starter, ultimate, plus, billing, curSym }: any) {
   return (
-    <TableContainer style={{ backgroundColor : '#081213' }} component={Paper}>
+    <TableContainer style={{ backgroundColor: '#081213' }} component={Paper}>
       <Table aria-label="simple table">
-        <TableHead>
+        <TableHead >
           <TableRow>
-            <TableCell>Feature</TableCell>
-            <TableCell>Free $0 /year</TableCell>
-            <TableCell>Starter ${starter} /year</TableCell>
-            <TableCell>Growth ${ultimate} /year</TableCell>
+            <TableCell>Pricing</TableCell>
+            <TableCell>{curSym}{starter} /mo (billed {billing}ly)</TableCell>
+            <TableCell>{curSym}{plus} /mo (billed {billing}ly)</TableCell>
+            <TableCell>{curSym}{ultimate} /mo (billed {billing}ly)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow>
-            <TableCell>Active surveys</TableCell>
-            <TableCell>1</TableCell>
-            <TableCell>5</TableCell>
-            <TableCell>10</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Users</TableCell>
-            <TableCell>1</TableCell>
-            <TableCell>Unlimited</TableCell>
-            <TableCell>Unlimited</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Response capacity / survey</TableCell>
-            <TableCell>50</TableCell>
-            <TableCell>2000</TableCell>
-            <TableCell>5000</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Response store limit</TableCell>
-            <TableCell>50</TableCell>
-            <TableCell>2000</TableCell>
-            <TableCell>5000</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Folders</TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Personalized assistance</TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>FeedbackSense  Branding</TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Questions in survey</TableCell>
-            <TableCell>Unlimited</TableCell>
-            <TableCell>Unlimited</TableCell>
-            <TableCell>Unlimited</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Detailed analysis</TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>AI analysis</TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Collection time / survey</TableCell>
-            <TableCell>Unlimited</TableCell>
-            <TableCell>Unlimited</TableCell>
-            <TableCell>Unlimited</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Notifications</TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Email support</TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Exclusive Features</TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-            <TableCell><CheckIcon sx={{ color: '#006DFF' }} /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Result export</TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell>Coming soon</TableCell>
-            <TableCell>Coming soon</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Custom logo</TableCell>
-            <TableCell><RemoveIcon /></TableCell>
-            <TableCell>Coming soon</TableCell>
-            <TableCell>Coming soon</TableCell>
-          </TableRow>
+          {
+            featureList.map((feature, index) => {
+              return (
+                <TableRow sx={{ backgroundColor: index % 2 === 0 ? '#000000' : '#081213' }} key={feature.name} >
+                  <TableCell>{feature.name}</TableCell>
+                  <TableCell>
+                    {feature.starter === 'yes' && <CheckIcon sx={{ color: '#006DFF' }} />}
+                    {feature.starter === 'no' && <RemoveIcon />}
+                    {feature.starter !== 'yes' && feature.starter !== 'no' && feature.starter}
+                  </TableCell>
+                  <TableCell>
+                    {feature.plus === 'yes' && <CheckIcon sx={{ color: '#006DFF' }} />}
+                    {feature.plus === 'no' && <RemoveIcon />}
+                    {feature.plus !== 'yes' && feature.plus !== 'no' && feature.plus}
+                  </TableCell>
+                  <TableCell>
+                    {feature.business === 'yes' && <CheckIcon sx={{ color: '#006DFF' }} />}
+                    {feature.business === 'no' && <RemoveIcon />}
+                    {feature.business !== 'yes' && feature.business !== 'no' && feature.business}
+                  </TableCell>
+                </TableRow>
+              )
+            })
+          }
         </TableBody>
       </Table>
     </TableContainer>
