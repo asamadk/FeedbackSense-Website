30ddede9b3e5c6aad9c3ccbba2a5519e4ea8da03
Vivek Singh <vivek.singh@liqvid.com>
2023-11-14 19:05:40 +0530

    updated website

--- src/Utils/DOMUtils.ts
+++
@@ -1,3 +0,0 @@
-export function setDocumentTitle(title : string) {
-    document.title = title;
-}
--- src/Utils/Sanity.ts
+++
@@ -1,7 +0,0 @@
-import sanityClient from '@sanity/client';
-
-export default sanityClient({
-    projectId: 'g4kwnxho', // you can find this in sanity.json
-    dataset: 'production', // e.g., 'production'
-    useCdn: true, // `false` if you want to ensure fresh data
-});
